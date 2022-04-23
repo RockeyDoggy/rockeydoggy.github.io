fc423100f64b38f8d56baf3dcfcdac3830071158
RockeyDoggy <juan_vsarts@hotmail.com>
2022-04-23 02:00:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -298,6 +298,23 @@ var tstInp = document.getElementById("locateTeste");
 
 tstInp.value = window.location.hash;
 //Via Link
+
+
+//On Whity
+  if(tstInp.value == "#whity"){
+    whitie();
+  }
+    if(tstInp.value == "#Whity"){
+      whitie();
+    }
+    if(tstInp.value == "#whitykun_"){
+      whitie();
+    }
+    if(tstInp.value == "#WhityKun_"){
+      whitie();
+    }
+
+
   //open King
   if(tstInp.value == "#kingo"){
     kingo();
@@ -370,20 +387,6 @@ tstInp.value = window.location.hash;
     }
     if(tstInp.value == "#draaaxxy"){
       mandrax();
-    }
-
-//On Whity
-  if(tstInp.value == "#whity"){
-    whitie();
-  }
-    if(tstInp.value == "#Whity"){
-      whitie();
-    }
-    if(tstInp.value == "#whitykun_"){
-      whitie();
-    }
-    if(tstInp.value == "#WhityKun_"){
-      whitie();
     }
   
 
