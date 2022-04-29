90fd795228abb3a832497c80651717a3ed9f84c4
RockeyDoggy <juan_vsarts@hotmail.com>
2022-04-28 22:06:30 -0300

    Create windowlocate.js

---
+++ js/windowlocate.js
@@ -0,0 +1,17 @@
+
+var tstInp = document.getElementById("locateTeste");
+tstInp.value = window.location.hash;
+
+//On Whity
+  if(tstInp.value == "#whity"){
+    whitie();
+  }
+    if(tstInp.value == "#Whity"){
+      whitie();
+    }
+    if(tstInp.value == "#whitykun_"){
+      whitie();
+    }
+    if(tstInp.value == "#WhityKun_"){
+      whitie();
+    }
