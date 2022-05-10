7b254eda92f137d525ab00bd325e0f8541f63c42
RockeyDoggy <juan_vsarts@hotmail.com>
2022-05-10 03:37:37 -0300

    Update boss.js

--- Bars/js/boss.js
+++ Bars/js/boss.js
@@ -1,23 +1,3 @@
-﻿//// Events will be sent when someone followers
-//// Please use event listeners to run functions.
-//
-//document.addEventListener('goalLoad', function(obj) {
-//  // obj.detail will contain information about the current goal
-//  // this will fire only once when the widget loads
-//  console.log(obj.detail);
-//  $('#title').html(obj.detail.title);
-//  $('#goal-current').text(obj.detail.amount.current);
-//  $('#goal-total').text(obj.detail.amount.target);
-//  $('#goal-end-date').text(obj.detail.to_go.ends_at);
-//});
-//
-//document.addEventListener('goalEvent', function(obj) {
-//  // obj.detail will contain information about the goal
-//  console.log(obj.detail);
-//  $('#goal-current').text(obj.detail.amount.current);
-//});
-
-
 // ------------------------------------------------------ //
 
 //Streamlabs Links
@@ -63,9 +43,9 @@ setTimeout(function refletir(){
   setTimeout(refletir, 1000);
   
   //var atual = document.getElementById("goal-current").innerHTML;
-  var atual = {AMOUNT};
+  var atual = "{AMOUNT}";
   //var total = document.getElementById("goal-total").innerHTML;
-  var total = {GOAL};
+  var total = "{GOAL}";
   var Paypal = document.getElementById("paypal").innerHTML;
   var Pix = document.getElementById("pix").innerHTML;
   
