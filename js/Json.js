4eebb94f5a3f02ad19e15520c930685a4b5611e6
RockeyDoggy <juan_vsarts@hotmail.com>
2022-05-29 04:22:25 -0300

    Update Json.js

--- js/Json.js
+++ js/Json.js
@@ -9,3 +9,9 @@ const kingoAvatar = "https://i.imgur.com/EYl3Pye.png";
 const foolsAvatar = "https://i.imgur.com/ccqMsY5.png";
 const juninAvatar = "https://i.imgur.com/1L6RW85.png";
 const sbekeAvatar = "https://i.imgur.com/gEqn6od.png";
+
+//Links
+const kingoLink = "https://rockeydoggy.github.io/#kingo";
+const foolsLink = "https://rockeydoggy.github.io/#fools";
+const juninLink = "https://rockeydoggy.github.io/#juninho";
+const sbekeLink = "https://rockeydoggy.github.io/#sbeeker";
