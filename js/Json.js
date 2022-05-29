20ffe4bcc272b4f07835e613c2ffbb3c556424a8
RockeyDoggy <juan_vsarts@hotmail.com>
2022-05-29 04:11:12 -0300

    Update Json.js

--- js/Json.js
+++ js/Json.js
@@ -1,3 +1,11 @@
 //Quem é?
 const kungKingo = "186a5s4d8w68951865"; //The Game
-const fooly     = "1868413w4818a6186a";
+const foolyFool = "1868413w4818a6186a";
+const juninSmol = "18458d48w6a8412w53";
+const sbekerLov = "17568521d58a4q8415";
+
+//Avatares
+const kingoAvatar = "https://i.imgur.com/EYl3Pye.png";
+const foolsAvatar = "https://i.imgur.com/ccqMsY5.png";
+const juninAvatar = "https://i.imgur.com/1L6RW85.png";
+const sbekeAvatar = "https://i.imgur.com/gEqn6od.png";
