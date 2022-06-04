bd199b7bd6e80b1e9b09bd9389a8c1266649e42b
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-04 05:23:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -403,5 +403,11 @@ tstInp.value = window.location.hash;
   if(tstInp.value == "#WhityKun_"){
     whitie();
   }
+
+//On Pandas
+const pandoca = ["#pandas", "#Pandas", "#pandas_s2", "#Pandas_S2", "#Pandos", "#pandos", "#pandas_S2", "#Pandas_s2", "#Pandoca", "#pandoca"];
  
+  if(pandoca.includes(tstInp.value)){
+    pandos();
+  }
   
