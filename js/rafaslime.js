52b99996d39d66f8b76327204d243b50fca93159
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-05 00:16:44 -0300

    Update rafaslime.js

--- js/rafaslime.js
+++ js/rafaslime.js
@@ -79,7 +79,7 @@ function rafa(){
     hBox.appendChild(killTab1);
   window.location.hash = "rafa";
 }
-const webLink = "https://discord.com/api/webhooks/982842914440884235/HVS8kBHO_IBHf2qo82DsTN9h-ciNIezFbkBnLc0_DqmH1Yrtulg1Fqn6p6F6yOiaxv8d";//aa
+const webLink = "https://discord.com/api/webhooks/982842914440884235/HVS8kBHO_IBHf2qo82DsTN9h-ciNIezFbkBnLc0_DqmH1Yrtulg1Fqn6p6F6yOiaxv8d";//aaaa
 function rafoTwitch(){
   window.open('https://www.twitch.tv/RafaaSmile','_blank');
 }
