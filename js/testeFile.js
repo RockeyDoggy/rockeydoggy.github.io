5c2b965ef282d661ec81803c92888e92afb09ceb
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-06 04:28:42 -0300

    Update testeFile.js

--- js/testeFile.js
+++ js/testeFile.js
@@ -26,8 +26,22 @@ var kingInfos = {
     item_detalhes:{
       vipItem_count: '1',
       rewardsItem_count: '5',
-      bossItem_count: '1',
+      bossItem_count: '0',
     }
+}
+var pandasInfo = {
+  nick: 'Pandas_S2',
+  display_nick: 'Pandas',
+  identification: pandostos ,
+  user_type: 'BOSS',
+  avatar_url: 'https://i.imgur.com/DB7cQaL.png',
+  link: 'https://rockeydoggy.github.io/#pandas',
+  user_item: [vipItem, rewardsItem, bossItem],
+  item_detalhes:{
+    vip: '1',
+    rewards: '5',
+    boss: '1',
+  }
 }
 
                     //Logme//
@@ -68,27 +82,66 @@ function versionUser(){
     nickID.style.pointerEvents = "auto";
     vistadecima.style.opacity = "1";
     vistadecima.display = "block";
+  }else{
+    vistadecima.style.opacity = "";
+    vistadecima.display = "";
   }
   //Ser King
   if(userID.value == kungKingo){
     //Areas Visiveis
-    nickID.innerHTML = kingInfos.nick;
-    typeID.innerHTML = kingInfos.user_type;
+    nickID.innerHTML      = kingInfos.nick;
+    typeID.innerHTML      = kingInfos.user_type;
     //Formulario
-    dataType.value = kingInfos.user_type;
-    dataName.value = kingInfos.nick;
-    dataNick.value = kingInfos.display_nick;
-    dataUserID.value = kingInfos.identification;
-    dataUserAvatar.value = kingInfos.avatar_url;
-    dataBossLink.value = kingInfos.link;
-    nickID.style.opacity = "1";
+    dataType.value        = kingInfos.user_type;
+    dataName.value        = kingInfos.nick;
+    dataNick.value        = kingInfos.display_nick;
+    dataUserID.value      = kingInfos.identification;
+    dataUserAvatar.value  = kingInfos.avatar_url;
+    dataBossLink.value    = kingInfos.link;
+    nickID.style.opacity  = "1";
   }
+  if(userID.value == pandasInfo.identification){
+    //ID Visiveis
+    nickID.innerHTML      = pandasInfo.nick;
+    typeID.innerHTML      = pandasInfo.user_type;
+    //Formulario
+    dataType.value        = pandasInfo.user_type;
+    dataName.value        = pandasInfo.nick;
+    dataNick.value        = pandasInfo.display_nick;
+    dataUserID.value      = pandasInfo.identification;
+    dataUserAvatar.value  = pandasInfo.avatar_url;
+    dataBossLink.value    = pandasInfo.link;
+    nickID.style.opacity  = "1";
+  };
+}
+function welMessage(){
+  const h5Message = document.createElement('h5');
+  const h6Message = document.createElement('h6');
+  const h7Message = document.createElement('h7');
+  const h8Message = document.createElement('H8');
+  h5Message.innerHTML = "Olá";
+  h6Message.innerHTML = "{NOME}";
+  h8Message.innerHTML = "Agora você pode resgatar seus itens e recompensas quando quiser.";
+  imagePlac.setAttribute("class", "cardImage");
+  errorScreen.style.display = "flex";
+  errorScreen.style.zIndex = "200";
+  errorBox.style.opacity = "1";
+  errorBox.append(h5Message, h6Message, imagePlac, h8Message);
+  errorBox.innerHTML = errorBox.innerHTML.replace(/{NOME}/g, dataNick.value);
+  setTimeout(function(){
+    errorBox.style.opacity = "0";
+    setTimeout(function(){
+      errorScreen.style.display = "";
+      errorScreen.style.zIndex = "";
+      errorBox.innerHTML = "";
+    }, 1000);
+  }, 3000)
 }
 
 //Lista de que? (Log)//
 function pspsCall(){
   var letters = document.getElementsByClassName("ltrs");
-  if(passInput.value.match("PINKUNKPIXIKINGUDO")){
+  if(passInput.value.match(kingudo)){
     userID.value = kungKingo;
     //nickID.innerHTML    = "S1rKing_";
     //typeID.innerHTML    = "BOSS";
@@ -101,12 +154,41 @@ function pspsCall(){
     versionUser();
     certaResposta();
     localStorage.setItem("LoggThis", kingInfos.identification);
-  }else if(passInput.value.match("STOVESOLUTIONS")){
-    userID.value = stevenUni;
+    //Message
+    imagePlac.setAttribute("src", "https://i.imgur.com/C8bdCDJ.png");
+    welMessage();
+    
+    //clear Password
+    setTimeout(function(){
+      passClose();
+    }, 7000);
+  }else if(passInput.value.match(stove)){
+    userID.value = steveInfos.identification;
     versionUser();
     certaResposta();
     localStorage.setItem("LoggThis", steveInfos.identification);
-  }else if(passInput.value.match("WRONGPASS?")){
+    //Message
+    imagePlac.setAttribute("src", "https://i.imgur.com/jpqYsWu.png");
+    welMessage();
+    
+    //clear Password
+    setTimeout(function(){
+      passClose();
+    }, 7000);
+  }else if(passInput.value.match(pandola)){
+    userID.value = pandasInfo.identification;
+    versionUser();
+    certaResposta();
+    localStorage.setItem("LoggThis", pandasInfo.identification);
+    //Message
+    imagePlac.setAttribute("src", "https://i.imgur.com/kLIT76F.png");
+    welMessage();
+    
+    //clear Password
+    setTimeout(function(){
+      passClose();
+    }, 7000);
+  }else if(passInput.value.match(prank)){
     //passAll.style.display = "none";
     passCheck.value = 2;
     warning.style.display = "initial";
@@ -114,28 +196,26 @@ function pspsCall(){
     warning.style.opacity = 1;
     pasScreen.style.opacity = 0;
     cnfPass.setAttribute('class', 'borderBtn den');
-    certaResposta();
-    
     setTimeout(function(){
       passAll.style.display = "none";
       warning.style.display = "none";
       warning.innerHTML = "";
       pasScreen.style.opacity = 1;
+      certaResposta();
+      passClose();
     }, 5000)
-  }else if(passInput.value.match("YIN-YANGEWEEBO")){
-    //for(i = 0; i <letters.length; i++){
-    //  letters[i].value = "";
-    //}
-    //sbekCheck.value= 1;
-    //lineSecret.value= 1;
-    //passCheck.value = 2;
-    //localStorage.setItem("lineSecret", lineSecret.value);
-    //  confSFX.play();//
-    //deniedSFX.pause();
-    //cnfPass.setAttribute('class', 'borderBtn confim');
+  }else if(passInput.value.match(sbk)){
+    sbekCheck.value= 1;
+    lineSecret.value= 1;
+    passCheck.value = 2;
+    localStorage.setItem("lineSecret", lineSecret.value);
+    confSFX.play();//
+    deniedSFX.pause();
+    cnfPass.setAttribute('class', 'borderBtn confim');
     certaResposta();
     setTimeout(function(){
         cnfPass.setAttribute('class', 'borderBtn');
+        passClose();
     }, 2000);
   }else{
     setTimeout(function(){
@@ -164,11 +244,14 @@ function pspsCall(){
 //Inventario Do Kingo
 function kingInventory(){
   if(userNick.innerHTML == "S1rKing_"){
-    const kingVipStorage = localStorage.getItem(nickID.innerHTML+vipID); //(nickID.innerHTML+vipID)
-    const kingRwStorage  = localStorage.getItem(nickID.innerHTML+rewardsID);  //(nickID.innerHTML+rewardsID)
+    const kingVipStorage = localStorage.getItem(userNick.innerHTML+vipID); //(nickID.innerHTML+vipID)
+    const kingRwStorage  = localStorage.getItem(userNick.innerHTML+rewardsID);  //(nickID.innerHTML+rewardsID)
     //Carregar Inventario
     inventory.append(florais, subInvent, vipItem, rewardsItem);//
     authVerify();
+    //Valores
+    vipItem.setAttribute("value", kingVipStorage);//
+    rewardsItem.setAttribute("value", kingRwStorage);
     
     //condições//
     if(nickID.innerHTML == userNick.innerHTML){
@@ -184,17 +267,12 @@ function kingInventory(){
     //Vip Quantidade Ajustes//
     if(isNaN(vipItem.getAttribute("value"))){
       vipItem.setAttribute("value", kingInfos.item_detalhes.vipItem_count);
-      localStorage.setItem(nickID.innerHTML+vipID, vipItem.getAttribute("value"));
-    }
-    else{
-      vipItem.setAttribute("value", kingVipStorage);//
+      localStorage.setItem(userNick.innerHTML+vipID, vipItem.getAttribute("value"));
     }
     //Reward Points Quatidade Ajustes
     if(isNaN(rewardsItem.getAttribute("value"))){
       rewardsItem.setAttribute("value", kingInfos.item_detalhes.rewardsItem_count);
-      localStorage.setItem(nickID.innerHTML+rewardsID, rewardsItem.getAttribute("value"));
-    }else{
-      rewardsItem.setAttribute("value", kingRwStorage);
+      localStorage.setItem(userNick.innerHTML+rewardsID, rewardsItem.getAttribute("value"));
     }
     emptyItem();
   }
@@ -204,10 +282,10 @@ function kingInventory(){
 function steveInventory(){
   if(userNick.innerHTML == steveInfos.nick){
     //Loads
-    const floraisLoad = localStorage.getItem(nickID.innerHTML+floraisID);
-    const giftLoad    = localStorage.getItem(nickID.innerHTML+giftID);
-    const vipLoad     = localStorage.getItem(nickID.innerHTML+vipID);
-    const rewardsLoad = localStorage.getItem(nickID.innerHTML+rewardsID);
+    const floraisLoad = localStorage.getItem(userNick.innerHTML+floraisID);
+    const giftLoad    = localStorage.getItem(userNick.innerHTML+giftID);
+    const vipLoad     = localStorage.getItem(userNick.innerHTML+vipID);
+    const rewardsLoad = localStorage.getItem(userNick.innerHTML+rewardsID);
     
     if(nickID.innerHTML == userNick.innerHTML){
       //Itens Disponiveis
@@ -238,19 +316,61 @@ function steveInventory(){
     //Quantidades caso o valor seja nulo//
     if(isNaN(floraisItem.getAttribute("value"))){
       floraisItem.setAttribute("value", steveInfos.item_detalhes.floraisItem_count);
-      localStorage.setItem(nickID.innerHTML+floraisID, floraisItem.getAttribute('value'));
+      localStorage.setItem(userNick.innerHTML+floraisID, floraisItem.getAttribute('value'));
     }
     if(isNaN(giftItem.getAttribute("value"))){
       giftItem.setAttribute("value", steveInfos.item_detalhes.giftItem_count);
-      localStorage.setItem(nickID.innerHTML+giftID, giftItem.getAttribute('value'));
+      localStorage.setItem(userNick.innerHTMLL+giftID, giftItem.getAttribute('value'));
     }
     if(isNaN(vipItem.getAttribute("value"))){
       vipItem.setAttribute("value", steveInfos.item_detalhes.vipItem_count);
-      localStorage.setItem(nickID.innerHTML+vipID, vipItem.getAttribute('value'));
+      localStorage.setItem(userNick.innerHTML+vipID, vipItem.getAttribute('value'));
     }
     if(isNaN(rewardsItem.getAttribute("value"))){
       rewardsItem.setAttribute("value", steveInfos.item_detalhes.rewardsItem_count);
-      localStorage.setItem(nickID.innerHTML+rewardsID, rewardsItem.getAttribute('value'));
+      localStorage.setItem(userNick.innerHTML+rewardsID, rewardsItem.getAttribute('value'));
+    }
+  }
+}
+
+
+          ///Inventario Do Pandas///
+function pandasInventory(){
+  if(userNick.innerHTML == pandasInfo.nick){
+    //Loads
+    const vipLoad     = localStorage.getItem(userNick.innerHTML+vipID);
+    const rewardsLoad = localStorage.getItem(userNick.innerHTML+rewardsID);
+    //Carregar Inventario
+    inventory.append(florais, subInvent, vipItem, rewardsItem);//
+    authVerify();
+    
+    //Carregar Quantidades
+    vipItem.setAttribute("value",  vipLoad);
+    rewardsItem.setAttribute("value",  rewardsLoad);
+    
+    //Se O Boss Estiver Logado
+    if(nickID.innerHTML == userNick.innerHTML){
+      //Itens Disponiveis
+      vipItem.setAttribute("onclick", "rwVip()");
+      rewardsItem.setAttribute("onClick", "claimRewards()");//
+      //Itens Para Exibir
+      inventory.style.filter = "none";
+      emptyItem();
+    }else{
+      vipItem.removeAttribute("onclick");
+      rewardsItem.removeAttribute("onClick");
+    }
+    
+    //Vip Quantidade Ajustes//
+    if(isNaN(vipItem.getAttribute("value"))){
+      vipItem.setAttribute("value", pandasInfo.item_detalhes.vip);
+      localStorage.setItem(userNick.innerHTML+vipID, vipItem.getAttribute("value"));
+    }
+    
+    //Rewards Quantidade Ajustes//
+    if(isNaN(rewardsItem.getAttribute("value"))){
+      rewardsItem.setAttribute("value", pandasInfo.item_detalhes.rewards);
+      localStorage.setItem(userNick.innerHTML+rewardsID, rewardsItem.getAttribute("value"));
     }
   }
 }
