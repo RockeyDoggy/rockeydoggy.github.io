c27f86553b488a3e4875ba109712ce12c8b2d72d
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-06 04:46:44 -0300

    Update Json.js

--- js/Json.js
+++ js/Json.js
@@ -50,3 +50,17 @@ var kingInfos = {
       bossItem_count: '0',
     }
 }
+var pandasInfo = {
+  nick: 'Pandas_S2',
+  display_nick: 'Pandas',
+  identification: pandostos ,
+  user_type: 'BOSS',
+  avatar_url: 'https://i.imgur.com/DB7cQaL.png',
+  link: 'https://rockeydoggy.github.io/#pandas',
+  user_item: [vipItem, rewardsItem, bossItem],
+  item_detalhes:{
+    vip: '1',
+    rewards: '5',
+    boss: '1',
+  }
+}
