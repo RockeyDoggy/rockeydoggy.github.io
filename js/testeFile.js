fc444a92fa480b607fbee63b83681ecef31db5e5
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-06 04:48:57 -0300

    Update testeFile.js

--- js/testeFile.js
+++ js/testeFile.js
@@ -1,17 +1,4 @@
-var pandasInfo = {
-  nick: 'Pandas_S2',
-  display_nick: 'Pandas',
-  identification: pandostos ,
-  user_type: 'BOSS',
-  avatar_url: 'https://i.imgur.com/DB7cQaL.png',
-  link: 'https://rockeydoggy.github.io/#pandas',
-  user_item: [vipItem, rewardsItem, bossItem],
-  item_detalhes:{
-    vip: '1',
-    rewards: '5',
-    boss: '1',
-  }
-}
+
 
                      //Logme//
 userID.value = localStorage.getItem("LoggThis");
