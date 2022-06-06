1ed1ee8c382c73fd78ec418ec2949821c9837152
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-06 05:18:07 -0300

    Update windowlocate.js

--- js/windowlocate.js
+++ js/windowlocate.js
@@ -18,8 +18,67 @@ sexo.value = window.location.hash;
 
 //On Pandas
 const pandao = ["#pandas", "#Pandas", "#pandas_s2", "#Pandas_S2", "#Pandos", "#pandos", "#pandas_S2", "#Pandas_s2", "#Pandoca", "#pandoca"];
- 
+  //open King
   if(pandao.includes(sexo.value)){
     pandos();
+    invLoad();
+  }
+
+
+//On Kingo
+const kingao = ["#king", "#King", "#kingo", "#Kingo", "#kingudo", "#Kingudo", "#Kingao", "#kingao", "#kingola", "#Kingola"];
+  //open King
+  if(kingao.includes(sexo.value)){
+    kingo();
+    invLoad();
+  }
+  
+//On Fools
+const foolzao = ["#Fools", "#fools", "#Fooly", "#fooly", "#Fools113", "#fools113", "#Foolzão", "#foolzão"];
+  //open Fools
+  if(foolzao.includes(sexo.value)){
+    fools();
+  }
+  
+//On Junin
+const junin = ["#Juninho", "#juninho", "#Junin", "#junin", "#junesco", "#Junesco", "#greque", "#Greque"];
+  //open Junin
+  if(junin.includes(sexo.value)){
+    junin();
+  }
+  
+//On Sbeeker
+const sbeeker = ["#Sbeeker", "#sbeeker", "#Sbekeer", "#sbekeer", "#SteveESneker", "#steveesneker", "#stevesneker", "#SteveSneker"];
+  //open Sbeeker
+  if(sbeeker.includes(sexo.value)){
+    sbeeker();
+  }
+  
+//On Raffa
+const raffas = ["#Rafa", "#rafa", "#rafaasmile", "#RafaaSmile", "#rafinha", "#Rafinha", "#filho01", "#Filho01"];
+  //open Rafa
+  if(raffas.includes(sexo.value)){
+    rafa();
+  }
+  
+//On Girl
+const girl = ["#girl", "#Girl", "#GirlStrangerFear", "#girlstrangerfear", "#BunBunny", "#bunbunny"];
+  //open Girl
+  if(girl.includes(sexo.value)){
+    girl();
+  }
+  
+//On Mandrax
+const mandrac = ["#mandrax", "#Mandrax", "#Draaaxxy", "#draaaxxy", "#Draxy", "#draxy"];
+  //open Mandrax
+  if(mandrac.includes(sexo.value)){
+    mandrax();
+  }
+
+//On Steve
+const stevo = ["#Steve", "#steve", "#sbevee_", "#Sbevee_", "#weebo", "#Weebo"];
+  //open Mandrax
+  if(stevo.includes(sexo.value)){
+    steve();
     invLoad();
   }
