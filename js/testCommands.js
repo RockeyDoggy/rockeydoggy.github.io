d14fbb1109ccda2f444c4ec9009ff6bb9e4362eb
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-06 04:27:21 -0300

    Create testCommands.js

---
+++ js/testCommands.js
@@ -0,0 +1,7 @@
+const kingudo = "PINKUNKPIXIKINGUDO";
+const stove   = "STOVESOLUTIONS";
+const pandola = "PANDULASUGARIDLPATOS";
+
+//Another
+const prank = "WRONGPASS?";
+const sbk   = "YIN-YANGEWEEBO";
