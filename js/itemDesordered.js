abde61e468ecc3446618958443dd2e184810122d
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-06 04:52:52 -0300

    Update itemDesordered.js

--- js/itemDesordered.js
+++ js/itemDesordered.js
@@ -1,15 +1,1 @@
 //const webLink = "";//aaaa
-var pandasInfo = {
-  nick: 'Pandas_S2',
-  display_nick: 'Pandas',
-  identification: pandostos ,
-  user_type: 'BOSS',
-  avatar_url: 'https://i.imgur.com/DB7cQaL.png',
-  link: 'https://rockeydoggy.github.io/#pandas',
-  user_item: [vipItem, rewardsItem, bossItem],
-  item_detalhes:{
-    vip: '1',
-    rewards: '5',
-    boss: '1',
-  }
-}
