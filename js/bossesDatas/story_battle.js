bfe9b479c2e7fa51465c11afb4db58006ebaed37
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-17 03:30:59 -0300

    Update story_battle.js

--- js/bossesDatas/story_battle.js
+++ js/bossesDatas/story_battle.js
@@ -18,4 +18,24 @@ const gui_infos_equipable = [
     count_id: 'fle',
     open: 'floraeyes()',
   },
+];    ///-----Gui Historico E Dados----///
+const pandas_infos_Story = [
+  rule1 = {
+    atirador:'Pandas_S2',
+    atitude: 'Derrotou',
+    vitima:  'Lucariojoga',
+  },
+]
+
+const pandas_infos_equipable = [
+  floraisEq = {
+    nome: "Flora'Eyes",
+    id: '1',
+    path: 'equipable.floraisEq',
+    icon_url: 'https://i.imgur.com/VbAIghD.png',
+    rarity: 'orangeItem',
+    tag: '',
+    count_id: 'fle',
+    open: 'floraeyes()',
+  },
 ];
