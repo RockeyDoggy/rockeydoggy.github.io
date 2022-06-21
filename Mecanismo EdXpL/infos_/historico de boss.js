d7ef8bb0feb9be3b3f25c3751e1b0cdb22a94d66
RockeyDoggy <juan_vsarts@hotmail.com>
2022-06-21 08:19:12 -0300

    Add files via upload

---
+++ Mecanismo EdXpL/infos_/historico de boss.js
@@ -0,0 +1,131 @@
+//////            //      Historico de boss      //          ////////////
+
+const FoolsStory = [
+    combat1 = {
+      player: 'Fools113',
+      action: 'Derrotou',
+      victim: 'RockeyDoggy'
+    },
+    combat2 = {
+      player: 'Fools113',
+      action: '✬',
+      victim: 'Se Tornou Boss'
+    },
+    combat3 = {
+      player: 'rGreque',
+      action: 'Derrotou',
+      victim: 'Fools113'
+    },
+    combat4 = {
+      player: 'Fools113',
+      action: '+50HP',
+      victim: 'Se Curou'
+    },
+    combat5 = {
+      player: 'RafaaSmile',
+      action: '-0.21HP',
+      victim: 'Fools113'
+    },
+    combat6 = {
+      player: 'Sbevee_',
+      action: '-3.48HP',
+      victim: 'Fools113'
+    },
+    combat7 = {
+      player: 'Fools113',
+      action: 'Derrotou',
+      victim: 'rGreque'
+    },
+    combat8 = {
+      player: 'Fools113',
+      action: '✬',
+      victim: 'Se Tornou Boss'
+    },
+    combat9 = {
+      player: 'Draaaxxy',
+      action: '-5HP',
+      victim: 'Fools113'
+    },
+    combat10 = {
+      player: 'Fools113',
+      action: '♚',
+      victim: 'Venceu'
+    },
+    combat11 = {
+      player: 'Fools113',
+      action: '-20HP',
+      victim: 'Sbeeker'
+    },
+  ]
+  
+  const JuninhoStory = [
+    combat1 = {
+      player: 'rGreque',
+      action: 'Derrotou',
+      victim: 'Fools113'
+    },
+    combat2 = {
+      player: 'rGreque',
+      action: '✬',
+      victim: 'Se Tornou Boss'
+    },
+    combat3 = {
+      player: 'Fools113',
+      action: '-50HP',
+      victim: 'rGreque'
+    },
+    combat4 = {
+      player: 'Fools113',
+      action: 'Derrotou',
+      victim: 'rGreque'
+    },
+  ]
+  
+  const KingStory = [
+    combat1 = {
+      player: 'S1rKing_',
+      action: '-1.11HP',
+      victim: 'Sbeeker'
+    },
+    combat2 = {
+      player: 'S1rKing_',
+      action: '-1HP',
+      victim: 'Se Tornou Boss'
+    },
+    combat3 = {
+      player: 'S1rKing_',
+      action: 'Derrotou',
+      victim: 'Kinguinha'
+    },
+  ]
+  
+  const PandasStory = [
+    combat1 = {
+      player: 'Pandas_S2',
+      action: 'Derrotou',
+      victim: 'Lucariojoga'
+    },
+  ]
+  
+  const GuiStory = [
+    combat1 = {
+      player: 'godiira',
+      action: 'Derrotou',
+      victim: 'Pandas_S2'
+    },
+  ]
+  
+  const EdneyStory = [
+    combat1 = {
+      player: 'edney_de_pijama',
+      action: 'Derrotou',
+      victim: 'godiira'
+    },
+    combat2 = {
+      player: 'edney_de_pijama',
+      action: '+749HP',
+      victim: 'Se Curou'
+    },
+  ]
+
+  //////            //      Historico dos não Boss      //          ////////////
