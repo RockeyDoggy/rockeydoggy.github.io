45bdbeed58ad61bbf86f6a5806521d130b33d381
RockeyDoggy <juan_vsarts@hotmail.com>
2022-10-13 13:37:49 -0300

    Add files via upload

---
+++ Fools Project/Files/import/data.js
@@ -0,0 +1,14 @@
+﻿const firebaseConfig = {
+  apiKey: "AIzaSyB4c3EFsOuuGpzv8Zt91Npwrh8JiUc0IqQ",
+  authDomain: "book-editor-59c7f.firebaseapp.com",
+  projectId: "book-editor-59c7f",
+  storageBucket: "book-editor-59c7f.appspot.com",
+  messagingSenderId: "979914178767",
+  appId: "1:979914178767:web:6852cb21515b565997b7ba",
+  measurementId: "G-CVPH00ZJEM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const fool = firebase.firestore();
