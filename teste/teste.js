16ff91c6991dd2515b14820f3e362806cde31c32
RockeyDoggy <juan_vsarts@hotmail.com>
2023-03-12 15:36:55 -0300

    Update teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -1,39 +1,20 @@
 
-      var twitch = new Twitch.Embed("twitch-embed", {
-        width: 400,
-        height: 300,
-        channel: "RockeyDoggy",
-        parent: ["rockeydoggy.github.io"]
-      });
+  // Inicialize o SDK da Twitch
+  Twitch.init({
+    clientId: 'xyphtivx2n7mw5ec9xtatlcijycovm'
+  });
 
-      function twitchLogin() {
-        var redirectUri = encodeURIComponent("https://rockeydoggy.github.io/auth/twitch");
-        window.location.href = "https://id.twitch.tv/oauth2/authorize?client_id=xyphtivx2n7mw5ec9xtatlcijycovm&redirect_uri=" + redirectUri + "&response_type=token&scope=user:read:email";
-      }
+  // Adicione um evento de clique ao botão de login
+  document.getElementById('twitch-login-button').addEventListener('click', function() {
+    // Chame a função de autenticação da Twitch
+    Twitch.login({
+      scope: 'user:read:email',
+      redirect_uri: 'https://rockeydoggy.github.io/testes'
+    });
+  });
 
-      function twitchCallback() {
-        var hash = window.location.hash.substring(1);
-        var tokenParams = hash.split("&");
-        var token = tokenParams[0].split("=")[1];
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://api.twitch.tv/helix/users", true);
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
-        xhr.setRequestHeader("Client-ID", "xyphtivx2n7mw5ec9xtatlcijycovm");
-        xhr.onload = function() {
-          if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            var displayName = response.data[0].display_name;
-            var userId = response.data[0].id;
-            document.getElementById("user-info").innerHTML = "Usuário autenticado: " + displayName + " (ID: " + userId + ")";
-          } else {
-            console.log("Erro ao obter informações do usuário.");
-          }
-        };
-        xhr.send();
-      }
-
-      document.getElementById("twitch-login-button").addEventListener("click", twitchLogin);
-
-      if (window.location.pathname === "/auth/twitch") {
-        twitchCallback();
-      }
+  // Adicione um listener para o evento de autenticação bem sucedida
+  Twitch.events.addListener('auth.login', function(user) {
+    // Exiba o nome de usuário autenticado na página
+    document.getElementById('username-input').value = user.display_name;
+  });
