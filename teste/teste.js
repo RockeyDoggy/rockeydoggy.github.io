1a831a119bbf3c1e4c523572bd844809c4bbf056
RockeyDoggy <juan_vsarts@hotmail.com>
2023-03-12 15:42:38 -0300

    Update teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -1,9 +1,10 @@
 
   // Inicialize o SDK da Twitch
-  Twitch.init({
-    clientId: 'xyphtivx2n7mw5ec9xtatlcijycovm'
+  window.addEventListener('load', function() {
+    Twitch.init({
+      clientId: 'xyphtivx2n7mw5ec9xtatlcijycovm'
+    });
   });
-
   // Adicione um evento de clique ao botão de login
   document.getElementById('twitch-login-button').addEventListener('click', function() {
     // Chame a função de autenticação da Twitch
