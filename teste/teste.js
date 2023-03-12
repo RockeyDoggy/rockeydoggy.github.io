d476ab3509f15648bae209d6b0c39061183e514e
RockeyDoggy <juan_vsarts@hotmail.com>
2023-03-12 16:34:16 -0300

    Update teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -1,36 +1,39 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const twitch = window.Twitch ? window.Twitch.ext : null;
 
-  if (!twitch) {
-    // Aguarda até que o objeto Twitch.ext seja definido pelo SDK da Twitch
-    setTimeout(() => {
-      const twitch = window.Twitch ? window.Twitch.ext : null;
+      var twitch = new Twitch.Embed("twitch-embed", {
+        width: 0,
+        height: 0,
+        channel: "RockeyDoggy",
+        parent: ["https://rockeydoggy.github.io"]
+      });
 
-      if (!twitch) {
-        console.error('Twitch SDK não encontrado.');
-        return;
+      function twitchLogin() {
+        var redirectUri = encodeURIComponent("https://rockeydoggy.github.io/testes.html");
+        window.location.href = "https://id.twitch.tv/oauth2/authorize?client_id=wmvc2pcoaa2g2cno4ldyackeh3l8t7&redirect_uri=" + redirectUri + "&response_type=token&scope=user:read:email";
       }
 
-      // Continue com o seu código aqui, como adicionar o evento onAuthorized e criar o botão de login da Twitch
-    }, 10000);
-  } else {
-    twitch.onAuthorized((auth) => {
-      // Exibe as informações de autenticação na div
-      const userInfo = document.getElementById('user-info');
-      userInfo.innerHTML = `Usuário autenticado: ${auth['user_id']}`;
-    });
+      function twitchCallback() {
+        var hash = window.location.hash.substring(1);
+        var tokenParams = hash.split("&");
+        var token = tokenParams[0].split("=")[1];
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://api.twitch.tv/helix/users", true);
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+        xhr.setRequestHeader("Client-ID", "wmvc2pcoaa2g2cno4ldyackeh3l8t7");
+        xhr.onload = function() {
+          if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            var displayName = response.data[0].display_name;
+            var userId = response.data[0].id;
+            document.getElementById("user-info").innerHTML = "Usuário autenticado: " + displayName + " (ID: " + userId + ")";
+          } else {
+            console.log("Erro ao obter informações do usuário.");
+          }
+        };
+        xhr.send();
+      }
 
-    // Cria o botão de autenticação da Twitch
-    const twitchLogin = new Twitch.LoginButton({
-      // Substitua CLIENT_ID pelo seu Client ID
-      client_id: 'wmvc2pcoaa2g2cno4ldyackeh3l8t7',
-      redirect_uri: 'https://rockeydoggy.github.io/testes.html',
-      scope: ['user_read'],
-      width: 200,
-      height: 40,
-      theme: 'dark'
-    });
-    twitchLogin.attach('#twitch-login');
-  }
-});
+      document.getElementById("twitch-login-button").addEventListener("click", twitchLogin);
 
+      if (window.location.pathname === "/auth/twitch") {
+        twitchCallback();
+      }
