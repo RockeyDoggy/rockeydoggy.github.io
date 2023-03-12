65a03f5f388cbdc9bdee5616268fa18b1478dd0b
RockeyDoggy <juan_vsarts@hotmail.com>
2023-03-12 16:17:43 -0300

    Update teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -14,24 +14,23 @@ window.addEventListener('DOMContentLoaded', () => {
       // Continue com o seu código aqui, como adicionar o evento onAuthorized e criar o botão de login da Twitch
     }, 10000);
   } else {
-      twitch.onAuthorized((auth) => {
-        // Exibe as informações de autenticação na div
-        const userInfo = document.getElementById('user-info');
-        userInfo.innerHTML = `Usuário autenticado: ${auth['user_id']}`;
-      });
-
-      // Cria o botão de autenticação da Twitch
-      const twitchLogin = new Twitch.LoginButton({
-        // Substitua CLIENT_ID pelo seu Client ID
-        client_id: 'g9ca0qr1bl5w8kmxss461l6i622ddy',
-        redirect_uri: 'https://rockeydoggy.github.io/testes.html',
-        scope: ['user_read'],
-        width: 200,
-        height: 40,
-        theme: 'dark'
-      });
-      twitchLogin.attach('#twitch-login');
+    twitch.onAuthorized((auth) => {
+      // Exibe as informações de autenticação na div
+      const userInfo = document.getElementById('user-info');
+      userInfo.innerHTML = `Usuário autenticado: ${auth['user_id']}`;
     });
+
+    // Cria o botão de autenticação da Twitch
+    const twitchLogin = new Twitch.LoginButton({
+      // Substitua CLIENT_ID pelo seu Client ID
+      client_id: 'xyphtivx2n7mw5ec9xtatlcijycovm',
+      redirect_uri: 'https://rockeydoggy.github.io/testes.html',
+      scope: ['user_read'],
+      width: 200,
+      height: 40,
+      theme: 'dark'
+    });
+    twitchLogin.attach('#twitch-login');
   }
 });
 
